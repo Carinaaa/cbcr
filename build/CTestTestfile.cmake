eff91a7c4e7366258d10690936a8c3a40e5e18ca
Carina Camara <xd.carina@yahoo.ro>
2023-10-27 21:50:27 +0300

    Added back the build

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: I:/Git_presetantions/code_run/cbcr
+# Build directory: I:/Git_presetantions/code_run/cbcr/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("I:/Git_presetantions/code_run/cbcr/build/hello_test[1]_include.cmake")
+subdirs("_deps/googletest-build")
